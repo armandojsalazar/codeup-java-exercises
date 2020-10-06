093634f3817f919ae9d6ceed7fe097c49b99eebf
armandojsalazar <armando.salazar89@gmail.com>
2020-10-06 11:34:53 -0500

    Feat: Working on Exercise #2

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,25 +1,24 @@
 package util;
-
-
 import java.util.Scanner;
 
 public class Input {
-    private Scanner scanner = new Scanner(System.in);
+    private Scanner sc = new Scanner(System.in);
 
+    String getString(){
+        System.out.println("Enter a word.");
+        String userInput = sc.nextLine();
+        return userInput;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
+    boolean yesNo(){
+        System.out.println("Do you like Codeup?");
+        String userInput = sc.nextLine();
+        if(userInput == "yes"){
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+        return yesNo();
+    }
 }
+
