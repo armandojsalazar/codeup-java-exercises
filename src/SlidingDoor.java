02908be7a0ce705201060a59ac749bf5c3f101cc
armandojsalazar <armando.salazar89@gmail.com>
2020-10-08 14:12:56 -0500

    Feat: Made a door and sliding door

--- src/SlidingDoor.java
+++ src/SlidingDoor.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class SlidingDoor {
+public class SlidingDoor extends Door {
+    public SlidingDoor(String description){
+        super(description);
+    }
+
+    public void open() {
+        System.out.println("Attempting to slide open %s door.");
+        super.open();
+    }
+
+    public void close() {
+        System.out.println("Attempting to slide close the door");
+        super.close();
+    }
 }
