ae1727ff7d170011c678a7246288f7b49d671d2f
armandojsalazar <armando.salazar89@gmail.com>
2020-10-09 09:11:10 -0500

    feat: completed lecture and working on exercise

--- src/DoorTest.java
+++ src/DoorTest.java
@@ -1,16 +1,31 @@
 public class DoorTest {
     public static void main(String[] args) {
-        Door metalDoor = new Door("metal");
-        System.out.println("metalDoor.isOpen() = " + metalDoor.isOpen());
-        metalDoor.close();
-        metalDoor.open();
-        System.out.println("metalDoor.isOpen() = " + metalDoor.isOpen());
+//        Door metalDoor = new Door("metal");
+//        System.out.println("metalDoor.isOpen() = " + metalDoor.isOpen());
+//        metalDoor.close();
+//        metalDoor.open();
+//        System.out.println("metalDoor.isOpen() = " + metalDoor.isOpen());
+//
+//
+//        SlidingDoor backGlassSlidingDoor = new SlidingDoor("back glass");
+//        System.out.println("backGlassSlidingDoor.isOpen() = " + backGlassSlidingDoor.isOpen());
+//        backGlassSlidingDoor.close();
+//        backGlassSlidingDoor.open();
+//        System.out.println("backGlassSlidingDoor.isOpen() = " + backGlassSlidingDoor.isOpen());
 
+//
+//        LockingDoor backDoor = new LockingDoor("back");
+//        backDoor.open();
+//        System.out.println("backdoor.isOpen() = " + backDoor.isOpen());
+//        System.out.println("backdoor.isOpen() = " + backDoor.isOpen());
+//        backDoor.unlock();
+//        backDoor.unlock();
+//        backDoor.open();
+//        backDoor.lock();
+//        backDoor.open();
+//        backDoor.close();
 
-        SlidingDoor backGlassSlidingDoor = new SlidingDoor("back glass");
-        System.out.println("backGlassSlidingDoor.isOpen() = " + backGlassSlidingDoor.isOpen());
-        backGlassSlidingDoor.close();
-        backGlassSlidingDoor.open();
-        System.out.println("backGlassSlidingDoor.isOpen() = " + backGlassSlidingDoor.isOpen());
+        Door[] doors = new Door[3];
+
     }
 }
---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,19 @@
+package shapes;
+
+public class Rectangle {
+    protected double length;
+    protected double width;
+
+    public Rectangle(double aLength, double aWidth){
+        this.length  = aLength;
+        this.width = aWidth;
+    }
+
+    public double getPerimeter(){
+        return (2 * this.length) + (2 * this.width);
+    }
+
+    public double getArea(){
+        return this.length * this.width;
+    }
+}
