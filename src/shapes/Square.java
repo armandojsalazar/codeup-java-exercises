e8a51ea203808c1f30511271463b685b9e2d2636
armandojsalazar <armando.salazar89@gmail.com>
2020-10-09 09:23:13 -0500

    feat: updated square class with double

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,13 +1,9 @@
 package shapes;
 
-
-
-
 public class Square extends Rectangle {
-    protected int side;
-    public Square(int side) {
+
+    public Square(double side) {
         super(side, side);
-        this.side = side;
 
     }
 }
