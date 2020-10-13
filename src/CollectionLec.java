cee0ab40f28d144ee850946c4a172d8f2adf4f19
armandojsalazar <armando.salazar89@gmail.com>
2020-10-13 10:04:28 -0500

    feat: created more examples with ArrayList using the Post class

--- src/CollectionLec.java
+++ src/CollectionLec.java
@@ -1,13 +1,21 @@
+import oopLec.Post;
+
 import java.util.ArrayList;
 
 public class CollectionLec {
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<>();
+        ArrayList<Post> posts = new ArrayList<>();
+
         System.out.println(names.size());
         names.add("Armando");
         System.out.println(names.size());
         names.add("Kelli");
         names.add("Kristan");
+
+        posts.add(new Post("Mando's Weekend", "Armando"));
+        posts.add(new Post("Mando's 2nd Weekend", "Rick"));
+        posts.add(new Post("Mando's 3rd Weekend", "Morty"));
 
         System.out.println("names.get(0) = " + names.get(0));
 //        System.out.println("names.get(1) = " + names.get(1));
@@ -17,5 +25,19 @@ public class CollectionLec {
             System.out.println("names.get(1) = " + names.get(1).toUpperCase());
         }
 
+        Post deletedPost = posts.remove(1);
+        deletedPost.printTitleAndAuthor();
+        System.out.println("posts.lastIndexOf(deletedPost) = " + posts.lastIndexOf(deletedPost));
+
+        if(!posts.isEmpty()){
+            for(Post post: posts){
+                post.printTitleAndAuthor();
+            }
+        }
+
+
     }
+
+
+
 }
