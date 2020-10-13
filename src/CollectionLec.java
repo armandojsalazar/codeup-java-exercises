762a7a7103c1ee1d62fcb3125dccc68665eac715
armandojsalazar <armando.salazar89@gmail.com>
2020-10-13 11:11:39 -0500

    feat: created simople examples for HashMaps with only strings

--- src/CollectionLec.java
+++ src/CollectionLec.java
@@ -1,6 +1,7 @@
 import oopLec.Post;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CollectionLec {
     public static void main(String[] args) {
@@ -35,9 +36,27 @@ public class CollectionLec {
             }
         }
 
-
+hashMapsPractice();
     }
 
+    public static void hashMapsPractice(){
+        HashMap<String, String> usernames = new HashMap<>();
+        usernames.put("daboskillz", "Armando J S");
+        usernames.putIfAbsent("daboskillz", "Armando Jesus S");
 
+        System.out.println("usernames.get(\"daboskillz\") = " + usernames.get("daboskillz"));
+
+        if(usernames.containsKey("ryanorsinger")){
+            System.out.println("usernames.get(\"ryanorsinger\") = " + usernames.get("ryanorsinger"));
+            System.out.println("Login into the system");
+        } else{
+            System.out.println("User not found");
+            System.out.println("Roll back transaction");
+            System.out.println("Ask again");
+        }
+
+        System.out.println("usernames.containsValue(\"Armando Jesus S\") = " + usernames.containsValue("Armando Jesus S"));
+        System.out.println("usernames.containsValue(\"Armando J S\") = " + usernames.containsValue("Armando J S"));
+    }
 
 }
